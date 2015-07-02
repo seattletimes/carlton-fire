a18854129412e4bb15af153775a68128c0f7ce6a
sgpotts <gpotts@news-0700ng.seatimes.com>
2015-07-02 11:47:34 -0700

    Updated colors and acres

--- src/js/palette.js
+++ src/js/palette.js
@@ -2,22 +2,22 @@ module.exports = function(value) {
   var day = new Date(value).getDate();
 
 if (day == 24) {
-    return '#FFFCD5';
-  } else if (day == 23) {
-    return '#FFEFC6';
-  } else if (day == 21) {
     return '#FFD990';
-  } else if (day == 20) {
+  } else if (day == 23) {
     return '#FCBB6D';
-  } else if (day == 19) {
+  } else if (day == 21) {
     return '#F99E49';
-  } else if (day == 18) {
+  } else if (day == 20) {
     return '#F47920';
-  } else if (day == 17) {
+  } else if (day == 19) {
+    return '#FA4C3C';
+  } else if (day == 18) {
     return '#E54E5F';
+  } else if (day == 17) {
+    return '#A05DA5';
   } else if (day == 16) {
-    return '#DD788A';
+    return '#522E91';
   } else if (day == 15) {
-    return 'A05DA5';
+    return '#260066';
   }
 };
