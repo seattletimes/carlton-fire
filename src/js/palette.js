8da289822e3251472346b0c5624270be4edc487b
Thomas Wilburn <news@thomaswilburn.net>
2015-07-06 16:04:08 -0700

    Switch to a lookup for palettes.

--- src/js/palette.js
+++ src/js/palette.js
@@ -1,23 +1,16 @@
+var colors = {
+  24: "#FFD990",
+  23: "#FCBB6D",
+  21: "#F99E49",
+  20: "#F47920",
+  19: "#FA4C3C",
+  18: "#E54E5F",
+  17: "#A05DA5",
+  16: "#522E91",
+  15: "#260066"
+};
+
 module.exports = function(value) {
   var day = new Date(value).getDate();
-
-if (day == 24) {
-    return '#FFD990';
-  } else if (day == 23) {
-    return '#FCBB6D';
-  } else if (day == 21) {
-    return '#F99E49';
-  } else if (day == 20) {
-    return '#F47920';
-  } else if (day == 19) {
-    return '#FA4C3C';
-  } else if (day == 18) {
-    return '#E54E5F';
-  } else if (day == 17) {
-    return '#A05DA5';
-  } else if (day == 16) {
-    return '#522E91';
-  } else if (day == 15) {
-    return '#260066';
-  }
+  return colors[day];
 };
